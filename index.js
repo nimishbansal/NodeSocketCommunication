afe85a9aa62a1c308dc49712d2643bff1782b074
nimish <nimish4july1998@gmail.com>
2019-05-31 23:56:35 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const socket = require("socket.io");
+const app = express();
+//
+// app.get('/', function (req, res) {
+//     res.send('hello world')
+// });
+
+const server = app.listen(4001, function ()
+{
+    console.log("listening to requests at port 4001")
+});
+
+//Static files
+app.use(express.static("public"));
+
+
+
+//Socket setup
+let io = socket(server);
+io.on('connection', function(socket){
+    console.log("connection");
+    socket.on('chat message', function(msg){
+        console.log(msg);
+    });
+    socket.emit('event', { hello: 'world' });
+});
